06900231eb061b3abe096d364bbed83d770b4e03
yoid2000 <paul@francis.com>
2025-04-11 21:16:20 +0200

    plots

--- compare/compare.py
+++ compare/compare.py
@@ -11,6 +11,8 @@ orig_files_dir = os.path.join('..', 'original_data_parquet')
 anon_files_dir = os.path.join('..', 'anon_data_parquet')
 work_files_dir = os.path.join('work_files')
 os.makedirs(work_files_dir, exist_ok=True)
+plots_dir = os.path.join('plots')
+os.makedirs(plots_dir, exist_ok=True)
 os.makedirs('slurm_out', exist_ok=True)
 
 
@@ -28,7 +30,7 @@ def do_work(job_num):
     # read in the corresponding anonymized file 
     my_file_anon = os.path.join(anon_files_dir, file_name)
     df_anon = pd.read_parquet(my_file_anon)
-    df_cntl = df_orig.sample(n=1000, random_state=42)
+    df_cntl = df_orig.sample(n=1000)
     df_orig = df_orig.drop(df_cntl.index)
     print(f"Created df_cntl with {len(df_cntl)} rows")
     print(f"Original df has {len(df_orig)} rows")
@@ -46,11 +48,60 @@ def do_work(job_num):
     brm.run_auto_attack()
 
 
-def do_plot():
-    pass
+def do_plots():
+    # read in all_secret_known.parquet file as pandas dataframe
+    df = pd.read_parquet("all_secret_known.parquet")
+    # make a new column called alc_floor where all alc values less than 0 are set to 0
+    df['alc_floor'] = df['alc'].clip(lower=-0.5)
+    idx = df.groupby(['secret_column', 'known_columns'])['alc'].idxmax()
+    df_best = df.loc[idx].reset_index(drop=True)
+    df_one = df[df['attack_recall'] == 1].reset_index(drop=True)
+    idx = df_one.groupby(['secret_column', 'known_columns'])['alc'].idxmax()
+    df_one = df_one.loc[idx].reset_index(drop=True)
+    # for each combination of secret_column and known_columns, that is in both
+    # df_best and df_one, make a new dataframe df_diff that contains the difference
+    # of 'alc' between df_best and df_one
+    # Merge df_best and df_one on 'secret_column' and 'known_columns'
+    df_merged = pd.merge(
+        df_best[['secret_column', 'known_columns', 'alc_floor']],
+        df_one[['secret_column', 'known_columns', 'alc_floor']],
+        on=['secret_column', 'known_columns'],
+        suffixes=('_best', '_one')
+    )
+    # Compute the difference of alc values
+    df_merged['alc_difference'] = df_merged['alc_floor_best'] - df_merged['alc_floor_one']
+    print("df_best alc:")
+    print(df_best['alc_floor'].describe())
+    print("df_one alc:")
+    print(df_one['alc_floor'].describe())
+    # describe alc_difference
+    print(df_merged['alc_difference'].describe())
 
-import os
-import pandas as pd
+    # make df_top which is df_merged where alc_floor >= 0.5
+    df_top = df_merged[df_merged['alc_floor_best'] >= 0.5]
+    print(f"There are {len(df_top)} rows with alc >= 0.5")
+    print(f"Top group described:")
+    print(df_top['alc_difference'].describe())
+
+    # make a seaborn scatterplot from df_top with alc_floor_best on x and alc_floor_one on y
+    plt.figure(figsize=(6, 3))
+    sns.scatterplot(data=df_top, y='alc_floor_best', x='alc_floor_one')
+    plt.ylabel('Worst-case ALC with recall')
+    plt.xlabel('ALC without recall')
+    plt.ylim(-0.05, 1.05)
+    plt.xlim(-0.55, 1.05)
+    # draw a diagonal line from (0,0) to (1,1)
+    #plt.plot([0, 1], [0, 1], color='blue', linestyle='--')
+    # draw a horizontal line at y=0.5
+    plt.axhline(y=0.5, color='red', linestyle='--')
+    # draw a vertical line at x=0.5
+    plt.axvline(x=0.5, color='red', linestyle='--')
+    plt.grid(True)
+    # tighten
+    plt.tight_layout()
+    plt.savefig(os.path.join(plots_dir, 'alc_best_vs_one.png'))
+    plt.savefig(os.path.join(plots_dir, 'alc_best_vs_one.pdf'))
+    plt.close()
 
 def do_gather():
     # List to store dataframes
@@ -88,7 +139,7 @@ def main():
                 quit()
             do_work(int(sys.argv[2]))
         elif sys.argv[1] == 'plot':
-            do_plot()
+            do_plots()
         elif sys.argv[1] == 'gather':
             do_gather()
     else:
