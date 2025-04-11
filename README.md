3443c0089c5b45c04dbeecbc921743e0f222f37e
yoid2000 <paul@francis.com>
2025-04-11 11:44:02 +0200

    Initial compare

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # alc-paper
+
+## Datasets
+
+The datasets used in the paper can be found in directory `original_data_parquet`
+
+
+### Code
+
+Directory `dependence` contains the code and results for generating the plot showing the effect of dependent rows in the data.
---
+++ compare/compare.py
@@ -0,0 +1,76 @@
+import sys
+import os
+from alc_attacks.best_row_match.brm_attack import BrmAttack
+import pandas as pd
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+import statistics
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from scipy.stats import bootstrap
+import json
+
+orig_files_dir = os.path.join('..', 'original_data_parquet')
+anon_files_dir = os.path.join('..', 'anon_data_parquet')
+work_files_dir = os.path.join('work_files')
+os.makedirs(work_files_dir, exist_ok=True)
+
+
+def do_work(job_num):
+    print(f"Job number {job_num} started.")
+    files_list = os.listdir(orig_files_dir)
+    print(files_list)
+    files_list_index = job_num % len(files_list)
+    file_name = files_list[files_list_index]
+    print(f"Processing file {file_name} with index {files_list_index}")
+    my_file = os.path.join(orig_files_dir, file_name)
+    # read in my_file parquet file as pandas dataframe 
+    df_orig = pd.read_parquet(my_file)
+    # read in the corresponding anonymized file 
+    my_file_anon = os.path.join(anon_files_dir, file_name)
+    df_anon = pd.read_parquet(my_file_anon)
+    df_cntl = df_orig.sample(n=1000, random_state=42)
+    df_orig = df_orig.drop(df_cntl.index)
+    print(f"Created df_cntl with {len(df_cntl)} rows")
+    print(f"Original df has {len(df_orig)} rows")
+    # strip suffix .parquet from file_name
+    file_name = file_name.split('.')[0]
+    attack_dir_name = f"{file_name}.{job_num}"
+    my_work_files_dir = os.path.join(work_files_dir, attack_dir_name)
+    os.makedirs(my_work_files_dir, exist_ok=True)
+    brm = BrmAttack(df_original=df_orig,
+                        df_control=df_cntl,
+                        df_synthetic=df_anon,
+                        results_path=my_work_files_dir,
+                        attack_name = attack_dir_name,
+                        )
+    brm.run_auto_attack()
+
+
+def do_plot():
+    pass
+
+def do_gather():
+    pass
+
+def main():
+    if len(sys.argv) > 1:
+        if sys.argv[1] == 'measure':
+            if len(sys.argv) != 3:
+                print("Usage: dependence.py measure <job_num>")
+                quit()
+            do_work(int(sys.argv[2]))
+        elif sys.argv[1] == 'plot':
+            do_plot()
+        elif sys.argv[1] == 'gather':
+            do_gather()
+    else:
+        print("No command line parameters were provided.")
+
+if __name__ == "__main__":
+    main()
